a2775fb4b333793e4394bdb47c5216391a3c9c76
Nathan <nsfaltermeier@BVT0006215.EMP.BV>
2016-01-19 13:53:45 -0600

    Added IOManager

---
+++ src/main/java/Blackop778/ChemCalc/Functions/IOManager.java
@@ -0,0 +1,22 @@
+package Blackop778.ChemCalc.Functions;
+
+public class IOManager
+{
+	private final boolean ECLIPSECONSOLEIO = true;
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	static void output(String output)
+	{
+
+	}
+
+	static String getInput()
+	{
+
+	}
+}
